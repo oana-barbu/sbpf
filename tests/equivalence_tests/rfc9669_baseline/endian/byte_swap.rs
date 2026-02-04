5b32e07ff70069012950da66106e0f04d1e3800e
Oana Barbu <akakob@gmail.com>
2026-02-04 21:01:00 +0000

    fix imports

--- tests/equivalence_tests/rfc9669_baseline/endian/byte_swap.rs
+++ tests/equivalence_tests/rfc9669_baseline/endian/byte_swap.rs
@@ -1,8 +1,23 @@
 #![allow(clippy::arithmetic_side_effects)]
 #![cfg(all(feature = "jit", not(target_os = "windows"), target_arch = "x86_64"))]
 
-use solana_sbpf::error::ProgramResult;
-use test_utils::{test_interpreter_and_jit_asm, TestContextObject};
+extern crate test_utils;
+
+use solana_sbpf::{
+    assembler::assemble,
+    ebpf,
+    error::ProgramResult,
+    memory_region::MemoryRegion,
+    program::BuiltinProgram,
+    static_analysis::Analysis,
+    verifier::RequisiteVerifier,
+    vm::{Config, ContextObject},
+};
+use std::sync::Arc;
+use test_utils::{
+    compare_register_trace, create_vm, test_interpreter_and_jit, test_interpreter_and_jit_asm,
+    TestContextObject,
+};
 
 #[test]
 fn test_be16_high() {
